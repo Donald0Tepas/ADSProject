7568bf6684cf70a28979c961becb5efb7180e086
Alfredo <st21i04001@usonsonate.edu.sv>
2024-04-12 20:56:10 -0600

    Se agregan validaciones para el modelo Profesor

--- ADSProject/Controllers/ProfesorController.cs
+++ ADSProject/Controllers/ProfesorController.cs
@@ -23,6 +23,12 @@ namespace ADSProject.Controllers
         {
             try
             {
+                // Verificar que todas las validaciones por atributo del modelo este correctas
+                if (!ModelState.IsValid)
+                {
+                    // En caso de no cumplir con todas las validaciones se procede a retornar una respuesta erronea
+                    return BadRequest(ModelState);
+                }
                 int contador = this.profesor.AgregarProfesor(profesor);
                 if (contador > 0)
                 {
@@ -50,6 +56,12 @@ namespace ADSProject.Controllers
         {
             try
             {
+                // Verificar que todas las validaciones por atributo del modelo este correctas
+                if (!ModelState.IsValid)
+                {
+                    // En caso de no cumplir con todas las validaciones se procede a retornar una respuesta erronea
+                    return BadRequest(ModelState);
+                }
                 int contador = this.profesor.ActualizarProfesor(IdProfesor, profesor);
 
                 if (contador > 0)
